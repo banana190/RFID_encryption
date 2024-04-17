02b6a9926c62ba19160e58952e6e89c69a89d60a
banana190 <playgametu@gmail.com>
2024-04-17 00:31:44 -0400

    HMAC derive AES key for RSA private key

--- ESP32_RFID/ESP32_RFID/main/publickey_reciever.c
+++ ESP32_RFID/ESP32_RFID/main/publickey_reciever.c
@@ -1,11 +1,3 @@
-/* BSD Socket API Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
@@ -21,10 +13,18 @@
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
 #include "mbedtls/sha256.h"
+#include "mbedtls/pkcs5.h"
+#include "mbedtls/md.h"
+#include "mbedtls/aes.h"
+
 
 #define PORT 9527
 #define CHAR_SET "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!~@#$^()_+=-"
 #define Signiture_length 16
+#define FLASH_ADDR 0x10000 // this is the RSA private key address
+#define AES_KEY_SIZE 32
+
+
 
 static const char *TAGg = "example";
 
@@ -96,7 +96,7 @@ void udp_server_task(void *pvParameters) {
                 ESP_LOGI("UDP", "Received %d bytes from %s:", len, addr_str);
                 ESP_LOGI("UDP", "%s", rx_buffer);
 
-                const char *tx_buffer = "Hello from ESP32";
+                const char *tx_buffer = "Hello from ESP32"; //TODO: change the message to RSA
                 source_addr.sin_port = htons(PORT);
                 for (int i = 0; i < 2; i++) {
                 int err = sendto(sock, tx_buffer, strlen(tx_buffer), 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
@@ -117,6 +117,7 @@ void udp_server_task(void *pvParameters) {
     }
     vTaskDelete(NULL);
 }
+
 
 void udp_broadcaster()
 {
@@ -140,8 +141,62 @@ void udp_broadcaster()
     inet_pton(AF_INET, "255.255.255.255", &dest_addr.sin_addr);
 }
 
-char Sign_with_SHA256()//WIP
+char RSA_Sign_with_SHA256()//WIP
 {
     char* signature = generate_random_string(Signiture_length);
     return signature;
+}
+
+char HMAC_derive_AES_for_RSA_privateKey() // this will always generate same AES key for RSA
+{
+    const char *salt = "Please stop stack overflowing thank you please god"; // I will change this in real application
+    const int iterations = 1000;   // I will change this in real application
+    unsigned char derived_key[32]; // AES-256
+    int ret;
+
+    ret = mbedtls_pkcs5_pbkdf2_hmac_ext(
+        MBEDTLS_MD_SHA256,  // SHA-256 as hash
+        NULL, 0,            // get key from efuse
+        (const unsigned char *)salt, strlen(salt), // add salt
+        iterations,
+        sizeof(derived_key) * 8, 
+        derived_key
+    );
+
+    if (ret == 0) {
+        ESP_LOGI("AES Key Derivation", "Key derived successfully.");
+    } else {
+        ESP_LOGE("AES Key Derivation", "Failed to derive key: %d", ret);
+    }
+
+    return derived_key;
+}
+
+void flash_writer() // this function might only be used for once since after I encrypted the RSA into flash
+{
+    char RSA_private_key[700]; // I'm using RSA-2048 so the private key is like 600-700 bytes long.
+    uint8_t encrypt_key[16]    // I wish it can be 16 bytes long... wish.
+    = AES_encrypter(RSA_private_key, sizeof(RSA_private_key), HMAC_derive_AES_for_RSA_privateKey()); // I'm gonna sleep so the size...yeah
+    int ret = spi_flash_write(FLASH_ADDR, encrypt_key, 16);
+    if (ret != ESP_OK) {
+        ESP_LOGE("Flash Writer", "Error writing data to flash: %d", ret);
+        return;
+    }
+}
+
+uint8_t AES_encrypter(const uint8_t *data, size_t data_len, const uint8_t *aes_key) // haven't test yet hope will work
+{
+    mbedtls_aes_context aes;
+    mbedtls_aes_init(&aes);
+    int ret = mbedtls_aes_setkey_enc(&aes, aes_key, AES_KEY_SIZE * 8);
+    if(ret != 0)
+    {
+        ESP_LOGE("AES","AES key set error");
+        return;
+    }
+
+    uint8_t encrypted_data[data_len]; // the size might need to be change
+    mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, data, encrypted_data);
+    mbedtls_aes_free(&aes);
+    return encrypted_data;
 }
